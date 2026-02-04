f502df32e61a21dc00cc60874b7cc3423f17e99c
Sonali-Patel98 <sonalipatel9845@gmail.com>
2026-02-04 12:12:21 +0530

    path

---
+++ app.js
@@ -0,0 +1,13 @@
+// const path=require('path');
+// const filepath="index.js";
+// console.log(path.dirname(filepath));
+
+
+// const filepath1="C:\\Users\\sonal\\OneDrive\\Desktop\\project\\Password_Generator";
+// console.log(path.dirname(filepath1));
+
+const path = require("path");
+
+const filepath1 = "C:/Users/sonal/OneDrive/Desktop/project/Password_Generator";
+console.log(path.dirname(filepath1));
+
